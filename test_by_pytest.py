d5ef9d4052b6137f22cb30869b69f90dc7d7a2cb
Zarten <2415072653@qq.com>
2022-06-21 00:07:37 +0800

    add fixture

--- test_by_pytest.py
+++ test_by_pytest.py
@@ -1,18 +1,25 @@
 import requests
 import pytest
 
+
+@pytest.fixture(scope="function")
+def conn_sql():
+    print("start connect sql...")
+    yield
+    print("end connect sql...")
+
+
 class TestFirst():
     def setup(self):
         print("it's executed--------------")
 
-    @pytest.mark.smoke
     def test_wayjd(self):
         appkey = "da39dce4f8aa52155677ed8cd23a6470"
         url = "https://way.jd.com/jisuapi/newSearch"
         params = {"keyword": "zarten",
                   "appkey": appkey
                   }
-        res = requests.get(url,params)
+        res = requests.get(url, params)
         # res = requests.post(url, json=params)
         print(res.status_code)
         print(res.json())
@@ -21,7 +28,12 @@ class TestFirst():
         url = "https://www.baidu.com"
         res = requests.get(url)
         print(res.status_code)
-        print(res.text)
+
+    def test_qq(self, conn_sql):
+        url = "https://www.qq.com"
+        res = requests.get(url)
+        print(res.status_code)
+
 
 if __name__ == '__main__':
     pytest.main([])
