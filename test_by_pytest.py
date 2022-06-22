1b4c6f2f695d781571c91f7418d2fb6a716cc567
Zarten <2415072653@qq.com>
2022-06-23 00:11:43 +0800

    add muti association with session

--- test_by_pytest.py
+++ test_by_pytest.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import requests
 import pytest
 
@@ -10,10 +13,14 @@ def conn_sql():
     yield
     print("end connect sql...")
 
+@pytest.fixture(scope="session",autouse=True)
+def clear_yaml():
+    YamlUtil().clear_asso_yaml()
 
 class TestFirst():
     def setup(self):
         print("it's executed--------------")
+
 
     def test_wayjd(self):
         appkey = "da39dce4f8aa52155677ed8cd23a6470"
@@ -29,13 +36,16 @@ class TestFirst():
     def test_baidu(self):
         url = "https://www.baidu.com"
         res = requests.get(url)
+        YamlUtil().write_asso_yaml({"baidu":res.status_code})
         print(res.status_code)
+
+
 
     def test_qq(self, conn_sql):
         url = "https://www.qq.com"
         res = requests.get(url)
         print(res.status_code)
-        value=YamlUtil().read_asso_yaml("charge")
+        value=YamlUtil().read_asso_yaml()
         print("charge of value:",value)
 
 
--- yaml_util.py
+++ yaml_util.py
@@ -2,11 +2,15 @@ import os
 import yaml
 
 class YamlUtil:
-    def read_asso_yaml(self,key):
+    def read_asso_yaml(self):
         with open(os.getcwd()+"/asso.yaml",mode="r",encoding="utf-8") as f:
             value =yaml.load(stream=f,Loader=yaml.FullLoader)
-            return value[key]
+            return value
 
     def write_asso_yaml(self,data):
-        with open(os.getcwd()+"/asso.yaml",mode="w",encoding="utf-8") as f:
+        with open(os.getcwd()+"/asso.yaml",mode="a",encoding="utf-8") as f:
             yaml.dump(data=data,stream=f,allow_unicode=True)
+
+    def clear_asso_yaml(self):
+        with open(os.getcwd()+"/asso.yaml",mode="w",encoding="utf-8") as f:
+            f.truncate()
