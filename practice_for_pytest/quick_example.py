db0a97998decb01c355535950282d2c85ec5a8dc
Zarten <2415072653@qq.com>
2022-07-06 23:24:13 +0800

    added quick some of example

--- practice_for_pytest/quick_example.py
+++ practice_for_pytest/quick_example.py
@@ -1,6 +1,21 @@
-def inc(x):
-    return x + 1
+import pytest
 
+class TestFirstCls:
 
-def test_answer():
-    assert inc(3) == 4
+    def inc(self,x):
+        return x + 1
+
+    def f(self):
+        raise SystemExit(1)
+
+    def test_answer(self):
+        assert self.inc(3) == 5
+
+    def test_mytest(self):
+        with pytest.raises(SystemExit):
+            self.f()
+
+    def test_one(self):
+        x="hello"
+        assert "l" in x
+
